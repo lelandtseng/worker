0f57a778152fe19c90045f37425d9e9513090c5a
Liang Zeng <lelandtseng@gmail.com>
2010-07-29 23:40:17 +0800

    worker

---
+++ work.js
@@ -0,0 +1,17 @@
+
+function work(func, args, callback){
+
+    var worker = new Worker("worker.js");
+    worker.onmessage = function(ev){
+        worker.terminate();
+        callback(ev.data);
+        
+    }
+    var src = {
+        'func': func.toString(),
+        'args': args
+    }.toSource();
+    
+    worker.postMessage(src);
+}
+
